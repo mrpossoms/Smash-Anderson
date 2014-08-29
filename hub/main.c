2239432666accf64b76cfcd93c40bccdfc488fe3
Kirk Roerig <mr.possoms@gmail.com>
2014-08-29 06:14:18 -0600

    Working on getting the hub to receive radio trasmissions and decode them properly

--- hub/main.c
+++ hub/main.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <smash-imu.h>
 #include <libNEMA.h>
 #include <ardutalk.h>
-#include <indicurses.h>
 #include <Controls.h>
+#include <smash-telemetry.h>
 
 #define GPS_DEV "/dev/ttyAMA0"
 #define IMU_DEV "/dev/ttyUSB0"
-#define I2C_DEV "/dev/i2c-1"
 
 void printGpsCoords(GpsState* state){
 	char buf[256];
@@ -22,8 +23,10 @@ void printGpsCoords(GpsState* state){
 int main(int argc, const char* argv[]){
 	int fd_gps    = -1; //lnConnect(GPS_DEV, B57600);
 	int fd_rotors = -1;
+	int fd_radio  = smashTelemetryInit("/dev/ttyUSB0");
 	char gps_buf[255] = {0};	
 	GpsState gps_st = {0};
+	RotorStates rotor_st = {0};
 
 	// start servo driver and check the status
 	printf("Preparing servo driver...");
@@ -37,38 +40,40 @@ int main(int argc, const char* argv[]){
 	//fd_gps    = lnConnect(GPS_DEV, B57600);
 	fd_rotors = smashSpeedInit("/dev/servoblaster");	
 
-	icInit();
-
 	while(1){
-		int hx = IC_TERM_WIDTH >> 1, hy = IC_TERM_HEIGHT >> 1;
-		int qx = hx >> 1;
-		unsigned char t = (unsigned char)((ORIENTATION.x + M_PI) * 40.58f);
-		unsigned char rotors[4] = {
-			t, t, t, t
-		};
+		int msgType = -1;
+		if(smashReceiveCode(fd_radio, &msgType)){
+			usleep(10000);
 
-		char buf[100];
-		sprintf(buf, "rotors=%d %d %f\n", fd_rotors, t, ORIENTATION.y);
-		smashSpeedSet(fd_rotors, rotors);
+			printf("Message type %d\n", msgType);
+
+			switch(msgType){
+				case MSG_CODE_THROTTLE:
+					{
+						RotorStates temp = {0};
+						if(!smashReceiveMsg(fd_radio, &temp)){
+							memcpy(&rotor_st, &temp, sizeof(RotorStates));
+						}
+					}
+					break;
+				default:
+					printf("Unrecognized message!\n");
+			}
+		} 	
+	
+		unsigned char t = rotor_st[0];
+		printf("Rotors = {%d, %d, %d, %d}\n", (int)t); 
+
+		smashSpeedSet(fd_rotors, rotor_st);
 		/*if(lnReadMsg(gps_buf, 255)){
 			lnParseMsg(&gps_st, gps_buf); 
 		}*/
-		
-		clear();
 
-		icText(2, 2, buf);
 		if(gps_st.Fix)
 			printGpsCoords(&gps_st);		
 
-		icCurrentChar = '.';
-
-		icCurrentChar = 'o';
-		icDial(qx, hy, 10, ORIENTATION.x);
-		icDial(hx, hy, 10, ORIENTATION.y);
-		icDial(hx + qx, hy, 10, ORIENTATION.z);
-		
+	
 		usleep(10000);	
-		icPresent();
 	}
 	
 	printf("Done\n");
