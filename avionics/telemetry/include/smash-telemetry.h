d7b7225b0d17fe419f830226b83624b23e10e92e
Kirk Roerig <mr.possoms@gmail.com>
2014-09-09 00:01:44 -0600

    Working simple test of i2c comms

--- avionics/telemetry/include/smash-telemetry.h
+++ avionics/telemetry/include/smash-telemetry.h
@@ -1,6 +1,14 @@
 #ifndef SMASH_TELEM
 #define SMASH_TELEM
 
+#define MSG_TYPE_ROTORS 0
+#define MSG_TYPE_ORI    1
+#define MSG_TYPE_LOC    2
+
+typedef unsigned char OrientationStates[3];
+typedef unsigned char RotorStates[4];
+typedef float         LocationStates[3];
+
 int  smashTelemetryInit(const char* dev, int addr);
 void smashTelemetryShutdown(int fd);
 
