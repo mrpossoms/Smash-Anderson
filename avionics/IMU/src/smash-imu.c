9c5d7bf186042e0dc10cf2411e0c997b8f9688ed
Kirk Roerig <mr.possoms@gmail.com>
2014-09-09 00:16:25 -0600

    Working on integrating ArduTalk as the IMU communication method

--- avionics/IMU/src/smash-imu.c
+++ avionics/IMU/src/smash-imu.c
@@ -3,17 +3,38 @@
 #include <pthread.h>
 #include <math.h>
 #include <ardutalk.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 
 //#define __IMU_DEBUG
 
 pthread_t IMU_THREAD;
 int IS_POLLING_IMU = 0;
 int IMU_FD = -1;
-
+int synched = 0;
 Vec3 ORIENTATION;
 
 void print(const char* str){
 	write(1, str, strlen(str));
+}
+
+int smashImuSync(const char* token){
+	int len = strlen(token), bytes = 0;
+	char received[len + 1];
+
+	received[len] = '\0';
+
+	while(bytes < len)
+		ioctl(IMU_FD, FIONREAD, &bytes);
+	//while(received[0] != '#'){
+	//	atRead(IMU_FD, received, 1);
+	//	write(1, received, 1);
+	//}
+	
+	atRead(IMU_FD, &received[0], len);
+	printf("r: %s\nt: %s\n", received, token);
+
+	return memcmp(received, token, len) == 0;
 }
 
 void* __IMUpoller(void* params){
@@ -26,14 +47,26 @@ void* __IMUpoller(void* params){
 }
 
 int smashImuInit(const char* dev){
+	int ret;
 	IMU_FD = atOpen(dev, 57600);
 	atConfig(AT_BIN | AT_NCHKSUM);
 
-	atWrite(IMU_FD, "#ob",  3); // Turn on binary output
-	atWrite(IMU_FD, "#o1",  3); // Turn on continuous streaming output
-	atWrite(IMU_FD, "#oe0", 4); // Disable error message output
-
 	sleep(3);
+
+	while(!synched){
+		atWrite(IMU_FD, "#ob\n",  4); // Turn on binary output
+		atWrite(IMU_FD, "#o1\n",  4); // Turn on continuous streaming output
+		atWrite(IMU_FD, "#oe0\n", 5); // Disable error message output
+		
+		tcflush(IMU_FD, TCIFLUSH);
+		atWrite(IMU_FD, "#s00\r\n", 6);
+		
+		// sync 
+		if(!smashImuSync("#SYNCH00\r\n")) continue;
+		synched = 1;
+	}
+
+	write(1, "Synched\n", 8);
 
 	IS_POLLING_IMU = 1;
 	if((ret = pthread_create(&IMU_THREAD, NULL, __IMUpoller, NULL)) < 0){
--- avionics/IMU/tests/testConnect.c
+++ avionics/IMU/tests/testConnect.c
@@ -2,13 +2,13 @@
 #include <indicurses.h>
 
 int main(void){
-	smashImuInit();
-	icInit();
+	smashImuInit("/dev/ttyUSB0");
+//	icInit();
 
 	while(1){
 		int hx = IC_TERM_WIDTH >> 1, hy = IC_TERM_HEIGHT >> 1;
 		int qx = hx >> 1;
-
+/*
 		clear();
 		icCurrentChar = '.';
 
@@ -16,10 +16,11 @@ int main(void){
 		icDial(qx, hy, 10, ORIENTATION.x);
 		icDial(hx, hy, 10, ORIENTATION.y);
 		icDial(hx + qx, hy, 10, ORIENTATION.z);
-		
-		usleep(1000);	
-		icPresent();
-//		printf("%f, %f, %f\n", ORIENTATION.x, ORIENTATION.y, ORIENTATION.z);
+*/		
+		usleep(10000);	
+//		icPresent();
+		printf("%f, %f, %f\n", ORIENTATION.x, ORIENTATION.y, ORIENTATION.z);
+
 	}
 
 	return 0;
