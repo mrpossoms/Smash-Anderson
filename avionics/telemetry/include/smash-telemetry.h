aebb3a79457fdbe79abae58e8e34bdddb5eb1656
mrpossoms <mr.possoms@gmail.com>
2014-09-14 19:38:33 -0400

    Converted telemetry to respond to status requests using a shared memory snapshot

--- avionics/telemetry/include/smash-telemetry.h
+++ avionics/telemetry/include/smash-telemetry.h
@@ -1,6 +1,8 @@
 #ifndef SMASH_TELEM
 #define SMASH_TELEM
 
+#include "smash-hub.h"
+
 #define MSG_CODE_THROTTLE 0x01
 #define	MSG_CODE_STATUS   0x02
 
@@ -10,16 +12,10 @@ typedef byte  OrientationStates[3];
 typedef byte  RotorStates[4];
 typedef float LocationStates[3];
 
-typedef struct{
-	float       yawPitchRoll[3];
-	float       latLongAlt[3];
-	RotorStates throttles;
-} SmashStatusMsg;
-
 int  smashTelemetryInit(const char* dev);
 void smashTelemetryShutdown(int fd);
 
-int smashTelSendStatus(int fd, SmashStatusMsg* status);
+int smashSendStatus(int fd, struct SmashState* status);
 
 int smashReceiveCode(int fd, int* type);
 int smashReceiveMsg (int fd, void* msg);
