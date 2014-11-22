7061a74841ef0119782f54fd895f205f79d8f9bc
Kirk <kirk@ColPossoms.(none)>
2014-11-22 15:49:27 -0500

    Added lock to smash-telemetry that makes radio usage thread safe

--- avionics/telemetry/src/smash-telemetry-connect.c
+++ avionics/telemetry/src/smash-telemetry-connect.c
@@ -4,13 +4,20 @@
 #include <stdlib.h>
 #include <ardutalk.h>
 
+pthread_mutex_t SMASH_TELEM_LOCK;
+
 int smashTelemetryInit(const char* dev){
 	int fd = -1;
 
 	if ((fd = atOpen(dev, 115200, AT_BIN | AT_NCHKSUM | AT_BLOCKING)) < 0) {
-		printf("Failed to open the telemetry bus.");
+		printf("Failed to open the telemetry bus.\n");
 		exit(1);
 	}
 	
+	if(pthread_mutex_init(&SMASH_TELEM_LOCK, NULL)){
+		printf("Initializing lock failed\n");
+		exit(2);
+	}
+
 	return fd;
 }
